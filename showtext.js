ed56bb54444f309542132ed36d1aef06a822816c
martingamm <martigam@stud.ntnu.no>
2011-04-20 05:33:48 +0200

    Now alt-text comes after the faces instead of before, also changed to grey textw

--- showtext.js
+++ showtext.js
@@ -1,6 +1,6 @@
 var faces = $("a");
 for (i = 0; i < faces.length; i++) {
-    if (faces[i].title && !faces[i].innerHTML {
-        faces[i].innerHTML = "( " + faces[i].title + " )";
+    if (faces[i].title && !faces[i].innerHTML) {
+        $(faces[i]).after("<span style='color:grey;'> " + faces[i].title + "</span>");
     }
 }
